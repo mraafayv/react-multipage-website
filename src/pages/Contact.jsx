336dca41e339bb8657c607f8bdf4dd9bf7d0ee01
mraafayv <raafayvohra.mrv@gmail.com>
2022-12-01 11:36:09 +0500

    initial router setup

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, facilis!
+        Alias totam voluptate amet doloremque iusto, minus fugit hic
+        reprehenderit libero consequuntur adipisci minima quas ab, placeat quam
+        deserunt doloribus.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, facilis!
+        Alias totam voluptate amet doloremque iusto, minus fugit hic
+        reprehenderit libero consequuntur adipisci minima quas ab, placeat quam
+        deserunt doloribus.
+      </p>
+    </div>
+  );
+}
