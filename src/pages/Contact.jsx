a5c3ec61bf16e1f61d00b7006b0d9c44c38abe07
mraafayv <raafayvohra.mrv@gmail.com>
2022-12-03 22:47:36 +0500

    added a redirect for invalid routes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 export default function Contact() {
+
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+  console.log(name);
+
   return (
     <div>
       <h2>Contact</h2>
